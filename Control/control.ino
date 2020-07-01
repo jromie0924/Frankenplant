f64c86b7bf2fac802176466a6810e666efb72a3a
Jackson Romie <jromie0924@gmail.com>
2020-07-01 17:40:42 -0600

    Test serial via USB

--- Control/control.ino
+++ Control/control.ino
@@ -1,12 +1,23 @@
+int counter = 0;
+
 void setup() {
   // LED_BUILDIN is an alias to pin 13 on the Arduino Uno.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(57600);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  if (counter == 5) {
+    counter = 0;
+  } else {
+    ++counter;
+  }
+  Serial.print("TEST");
+  Serial.println(counter);
+  delay(500);
+//  digitalWrite(LED_BUILTIN, HIGH);
+//  delay(1000);
+//
+//  digitalWrite(LED_BUILTIN, LOW);
+//  delay(1000);
 }
