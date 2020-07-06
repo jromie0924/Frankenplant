66c38fa1569b1815f72b3174b640475a8716b65f
Jackson Romie <jromie0924@gmail.com>
2020-07-06 11:53:05 -0600

    Updates to arduino code - pump operates in bursts, standardized serial output and put it on 9600 bit rate.

--- Control/control.ino
+++ Control/control.ino
@@ -1,16 +1,22 @@
-int airValue = 600;
-int waterValue = 230;
+const int AIR_VALUE = 600;
+const int WET_VALUE = 220;
+const int PUMP_TIMER = 2000;
+const int PAUSE_TIME = 3000;
+const int BAUD = 9600;
+const int WATERING_THRESHOLD = 100;
+const int MONITORING_THRESHOLD = 30;
 
 int soilMoistureAnalogReading = 0;
 int soilMoisturePercentage = 0;
+int dryReading = 0;
 
-bool pumpOn = false;
 int moistureSensor = A2;
+
 int waterRelay = 9;
 
 void setup() {
-  Serial.begin(57600);
-  
+  Serial.begin(BAUD);
+
   pinMode(moistureSensor, INPUT);
   pinMode(waterRelay, OUTPUT);
 
@@ -19,22 +25,56 @@ void setup() {
 }
 
 void loop() {
-  soilMoistureAnalogReading = analogRead(moistureSensor);
-  soilMoisturePercentage = map(soilMoistureAnalogReading, airValue, waterValue, 0, 100);
-  Serial.println(soilMoistureAnalogReading);
-  Serial.print(soilMoisturePercentage);
-  Serial.println("% moisture\n");
-
-  if (soilMoisturePercentage < 30) {
-    togglePump(true);
-  } else if (soilMoisturePercentage > 90) {
-    togglePump(false);
+  bool needsWater = checkMoisture(false);
+  if (needsWater) {
+    handleLowMoisture();
   }
-  
   delay(500);
+}
+
+void handleLowMoisture() {
+  int counter = 0;
+  bool cont = true;
+  dryReading = soilMoisturePercentage;
+
+  while (cont) {
+    ++counter;
+    togglePump(true);
+    delay(PUMP_TIMER);
+    togglePump(false);
+    delay(PAUSE_TIME);
+    cont = checkMoisture(true);
+    delay(500);
+  }
+
+  sendSerialMessage(counter * PUMP_TIMER, true);
+}
+
+bool checkMoisture(bool isWatering) {
+  const int comparator = isWatering ? WATERING_THRESHOLD : MONITORING_THRESHOLD;
+  int previousPercentage = soilMoisturePercentage;
+  soilMoistureAnalogReading = analogRead(moistureSensor);
+  soilMoisturePercentage = map(soilMoistureAnalogReading, AIR_VALUE, WET_VALUE, 0, 100);
+  if (soilMoisturePercentage != previousPercentage) {
+    sendSerialMessage(0, false);
+  }
+  return soilMoisturePercentage < comparator;
 }
 
 void togglePump(bool on) {
   int analogValue = on ? 0 : 255;
   analogWrite(waterRelay, analogValue);
+  delay(200);
+}
+
+void sendSerialMessage(int duration, bool isWatering) {
+  if (isWatering) {
+    Serial.print(dryReading);
+    Serial.print(",");
+    Serial.print(soilMoisturePercentage);
+    Serial.print(",");
+    Serial.println(duration);
+  } else {
+    Serial.println(soilMoisturePercentage);
+  }
 }
